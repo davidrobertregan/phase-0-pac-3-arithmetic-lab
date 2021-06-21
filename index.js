4f4e86acdd8320a8d8ea9c27c7cca260eb41a4b2
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-06-21 11:37:15 -0600

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+function add(x,y){
+    return(x + y)
+}
+
+function subtract(a,b){
+    return(a - b)
+}
+
+function multiply(a,b){
+    return(a * b)
+}
+
+function divide(a,b){
+    return(a / b)
+}
+
+function increment(n){
+    return(n += 1)
+}
+
+function decrement(n){
+    return(n -= 1)
+}
+
+function makeInt(string){
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string){
+    return parseFloat(string)
+}
